ea08e4be5d737b3121c0fd831fc6f033c9f14134
deboraverssao-arch <debora.verssao@escola.pr.gov.br>
2026-03-25 18:49:51 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,8 @@ let tempCelsius = 18.5;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempKenvin = tempCelsius + 273.15;
+
+console.log(`A temperatura de ${cidade} está ${tempCelsius} Cº`);
+console.log(`Fahrenheit: ${tempFahrenheit} Fº`);
+console.log(`Kelvin: ${tempKenvin.toFixed(1)} Kº`);
+
