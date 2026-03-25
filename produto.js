066add1ca8b3a757f016a10e02df0c15a543f537
deboraverssao-arch <debora.verssao@escola.pr.gov.br>
2026-03-25 18:28:43 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = "Mochila impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`´O produto é: ${produto}. `);
+console.log(`A ${produto} custa R$ ${preco.toFixed}.`);
+console.log(`O categoria de ${produto} é ${categoria}.`);
