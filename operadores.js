96bc2324606e0a59463cbade6cb200c282ad33c4
deboraverssao-arch <debora.verssao@escola.pr.gov.br>
2026-03-31 17:36:37 +0000

    versao1

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//OPERADOR E &&
+let maiorIdade = false;
+let possuiDocumento = false;
+
+if (maiorIdade && possuiDocumento) {
+    console.log('pode entrar na festa! ')
+} else {
+    console.log('Não pode entrar na festa.')
+};
+
+//OPERADOR OU ||
+let cartaoCretido = false;
+let dinheiro = true;
+
+if (cartaoCretido || dinheiro) {
+    console.log('Pode fazer a compra.')
+} else {
+    console.log('Não pode fazer a compra.')
+};
+
+//OPERADOR NOT !
+let  estaChovendo = true;
+
+if (!estaChovendo) {
+    console.log('Pode sair sem guarda-chuva.')
+}else {
+    console.log('Leve o guarda-chuva.')
+};
