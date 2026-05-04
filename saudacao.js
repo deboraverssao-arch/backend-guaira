21a9a5f30b7cd350ff2e2eed764c9214d86491a7
deboraverssao-arch <debora.verssao@escola.pr.gov.br>
2026-05-04 19:19:21 +0000

    versao1

---
+++ saudacao.js
@@ -0,0 +1,8 @@
+let filaEspera = ["Ana", "Bruno", "Carlos"];
+
+console.log(filaEspera);
+filaEspera.push("Diego"); //Adiciona um elemento no final da
+console.log(filaEspera);
+
+filaEspera.shift();
+console.log(filaEspera);
