ce3083ba2f78071f34c201eefb574ba9935dc4cf
deboraverssao-arch <debora.verssao@escola.pr.gov.br>
2026-05-11 19:10:54 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+//🔵 Declare 5 variáveis de um perfil de um jogador
+
+let nomeJogador = "Gavi"
+let posição = "Meia"
+let idade = "21"
+let numeroCamisa = "6"
+let time ="Barcelona"
+
+console.log(`O jogador ${nomeJogador} Joga no ${time}`);
